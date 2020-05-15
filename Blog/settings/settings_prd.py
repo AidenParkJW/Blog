c3e2d35247a993dbbaff54663787c100f8402b0d
Administrator <Administrator@HomeNote>
2020-05-16 00:59:35 +0900

    upgrade Django

--- Blog/settings/settings_prd.py
+++ Blog/settings/settings_prd.py
@@ -15,17 +15,12 @@ INTERNAL_IPS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'sql_server.pyodbc',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': PROPERTIES["database"]["name"],
         'HOST': PROPERTIES["database"]["host"],
         'PORT': PROPERTIES["database"]["port"],
         'USER': PROPERTIES["database"]["user"],
         'PASSWORD': PROPERTIES["database"]["password"],
-        'OPTIONS': {
-            'driver': 'SQL Server Native Client 11.0',
-            'MARS_Connection': True,
-            'driver_supports_utf8': True,
-        }
     }
 }
 
