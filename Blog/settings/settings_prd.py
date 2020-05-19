56f6942b70759f3ef3fbe56f6c865e9bdefcaee6
Administrator <Administrator@HomeNote>
2020-05-20 01:05:33 +0900

    .

--- Blog/settings/settings_prd.py
+++ Blog/settings/settings_prd.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["blog.anzinda.com"]
+ALLOWED_HOSTS = ["blog.daonelab.com", "blog.home.daonelab.com", "blog.anzinda.com"]
 
 # debug toolbar
 INTERNAL_IPS = [
