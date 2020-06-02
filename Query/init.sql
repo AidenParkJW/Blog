a24a391542c6f3a6c1712aa8d5c38db527d31e5d
Administrator <Administrator@HomeNote>
2020-06-02 23:38:33 +0900

    .

--- Query/init.sql
+++ Query/init.sql
@@ -1,77 +1,53 @@
-DELETE FROM DJANGO_MIGRATIONS WHERE APP IN ('post', 'menu');
+select *
+from blog_post;
 
-SELECT * INTO BLOG_MENU_BACK
-FROM BLOG_MENU;
-
-SELECT * INTO BLOG_POST_BACK 
-FROM BLOG_POST;
-
-DROP TABLE BLOG_POST;
-DROP TABLE BLOG_MENU;
-
-SET IDENTITY_INSERT [BLOG].[BLOG_MENU] ON;
-INSERT INTO [BLOG].[BLOG_MENU]
-           ([MENU_UID]
-           ,[MENU_NAME]
-           ,[MENU_DESC]
-           ,[MENU_URL]
-           ,[MENU_SORT_ORDER]
-           ,[MENU_ISENABLED]
-           ,[MENU_CRTE_DT]
-           ,[MENU_MDFY_DT]
-           ,[LFT]
-           ,[RGHT]
-           ,[TREE_ID]
-           ,[LEVEL]
-           ,[MENU_CRTE_USER_ID]
-           ,[MENU_MDFY_USER_ID]
-           ,[MENU_UP_UID]
-           ,[SITE_ID])
-SELECT      [MENU_UID] 
-           ,[MENU_NAME]
-           ,[MENU_DESC]
-           ,[MENU_URL]
-           ,[MENU_SORT_ORDER]
-           ,[MENU_ISENABLED]
-           ,[MENU_CRTE_DT]
-           ,[MENU_MDFY_DT]
-           ,[LFT]
-           ,[RGHT]
-           ,[TREE_ID]
-           ,[LEVEL]
-           ,[MENU_CRTE_USER_ID]
-           ,[MENU_MDFY_USER_ID]
-           ,[MENU_UP_UID]
-           ,[SITE_ID]
-FROM BLOG.BLOG_MENU_BACK;
-SET IDENTITY_INSERT [BLOG].[BLOG_MENU] OFF
-
-
-SET IDENTITY_INSERT [BLOG].[BLOG_POST] ON;
-INSERT INTO [BLOG].[BLOG_POST]
-           ([POST_UID]
-           ,[POST_TITLE]
-           ,[POST_SLUG]
-           ,[POST_CONTENT]
-           ,[POST_VIEWS]
-           ,[POST_ISENABLED]
-           ,[POST_TAG]
-           ,[POST_CRTE_DT]
-           ,[POST_MDFY_DT]
-           ,[MENU_UID]
-           ,[POST_CRTE_USER_ID]
-           ,[POST_MDFY_USER_ID])
-SELECT      [POST_UID]
-           ,[POST_TITLE]
-           ,[POST_SLUG]
-           ,[POST_CONTENT]
-           ,[POST_VIEWS]
-           ,[POST_ISENABLED]
-           ,[POST_TAG]
-           ,[POST_CRTE_DT]
-           ,[POST_MDFY_DT]
-           ,[MENU_UID]
-           ,[POST_CRTE_USER_ID]
-           ,[POST_MDFY_USER_ID]
-FROM [BLOG].[BLOG_POST_BACK];
-SET IDENTITY_INSERT [BLOG].[BLOG_POST] OFF;
+insert into blog_post
+select  bod_idx         as post_uid
+,       bod_title       as post_title
+,       bod_slug        as post_slug
+,       con_content     as post_content
+,       bod_readcnt     as post_views
+,       true            as post_isEnabled
+,       bod_tag         as post_tag
+,       con_regdate     as post_crte_dt
+,       con_moddate     as post_mdfy_dt
+,       case cat_idx
+            when 11 then 32
+            when 12 then 33
+            when 13 then 26
+            when 14 then 24
+            when 15 then 29
+            when 16 then 17
+            when 17 then 18
+            when 18 then 15
+            when 19 then 27
+            when 20 then 23
+            when 24 then 41
+            when 25 then 14
+            when 28 then 20
+            when 40 then 31
+            when 41 then 34
+            when 42 then 40
+            when 44 then 26
+            when 45 then 16
+            when 47 then 10
+            when 48 then 10
+            when 49 then 13
+            when 51 then 21
+            when 53 then 22
+            when 55 then 37
+            when 57 then 10
+            when 58 then 30
+            when 59 then 38
+            when 60 then 11
+            when 61 then 28
+            when 63 then 19
+            when 64 then 12
+        end             as menu_uid
+,       1               as post_crte_user_id
+,       1               as post_mdfy_user_id
+from blog_post_mig
+where cat_idx not in (2, 5, 23, 26)
+order by cat_idx, bod_idx
+;
+commit;
